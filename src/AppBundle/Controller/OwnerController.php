7b466ee0500371ff2f6bc8fc1b83555211b94c2c
Fidel Reyes <freyesllerena33@gmail.com>
2024-05-02 10:55:00 +0200

    add routes & controller Owners

---
+++ src/AppBundle/Controller/OwnerController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class OwnerController extends AbstractController
+{
+    public function index(): Response
+    {
+        // Code pour gérer la requête de la route "owner"
+        return $this->render('owner/index.html.twig');
+    }
+
+    public function create(): Response
+    {
+        return $this->render('owner/add.html.twig');
+    }
+}
